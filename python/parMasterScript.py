ba8634ecc74e0154a004eabac1fb555d34f5f606
nmvenuti <nmv7de@virgina.edu>
2016-06-07 09:20:53 -0400

    Parallel development

--- python/parMasterScript.py
+++ python/parMasterScript.py
@@ -76,72 +76,81 @@ def textAnalysis(paramList):
     #Append outputs to masterOutput
     return(['_'.join(groupId)]+sentimentList+judgementAvg+[avgSD])
 
-###############################
-#####Raw File List Extract#####
-###############################
-
-#Get file list for documents
-rawPath = './data_dsicap/'
-groupList=['DorothyDay','JohnPiper','MehrBaba','NaumanKhan','PastorAnderson',
-           'Rabbinic','Shepherd','Unitarian','WBC']
-rawFileList=[]
-for groupId in groupList:
-    for dirpath, dirnames, filenames in os.walk(rawPath+groupId+'/raw'):
-        for filename in [f for f in filenames ]:
-            if '.txt' in filename:
-                rawFileList.append([groupId,os.path.join(dirpath, filename)])
-                
-###############################                
-#####Set up random binning#####
-###############################
-                
-#Loop through each group and create sub bins
-groupSize=10
-testSplit=0.1
-targetWordCount=10
-cocoWindow=6
-svdInt=50
-cvWindow=6
-simCount=1000
-fileList=[]
-for groupId in groupList:
-    subGroup=[x for x in rawFileList if groupId == x[0]]
-    randomSample=list(np.random.choice(range(len(subGroup)),size=len(subGroup),replace=False))
-    splitIndex=int((1-testSplit)*len(subGroup))
-    groupId=['train'+ "%02d" %int(i/groupSize) if i<splitIndex else 'test'+ "%02d" %int((i-splitIndex)/groupSize) for i in randomSample]
+def runMaster(rawPath,groupList,groupSize,testSplit,targetWordCount,cocoWindow,svdInt,cvWindow,simCount,nCores):
+    ###############################
+    #####Raw File List Extract#####
+    ###############################
+    rawFileList=[]
+    for groupId in groupList:
+        for dirpath, dirnames, filenames in os.walk(rawPath+groupId+'/raw'):
+            for filename in [f for f in filenames ]:
+                if '.txt' in filename:
+                    rawFileList.append([groupId,os.path.join(dirpath, filename)])
+                    
+    ###############################                
+    #####Set up random binning#####
+    ###############################
+                    
+    #Loop through each group and create sub bins
+    fileList=[]
+    for groupId in groupList:
+        subGroup=[x for x in rawFileList if groupId == x[0]]
+        randomSample=list(np.random.choice(range(len(subGroup)),size=len(subGroup),replace=False))
+        splitIndex=int((1-testSplit)*len(subGroup))
+        groupId=['train'+ "%02d" %int(i/groupSize) if i<splitIndex else 'test'+ "%02d" %int((i-splitIndex)/groupSize) for i in randomSample]
+        
+        fileList=fileList+[[subGroup[i][0],subGroup[i][1],groupId[i]] for i in range(len(subGroup))]
     
-    fileList=fileList+[[subGroup[i][0],subGroup[i][1],groupId[i]] for i in range(len(subGroup))]
-
-fileDF=pd.DataFrame(fileList,columns=['group','filepath','subgroup'])
-
-
-#Get set of subgroups
-subgroupList=[ list(y) for y in set((x[0],x[2]) for x in fileList) ]
-
-#Make output directory
-runDirectory='./pythonOutput/'+ time.strftime("%c")
-os.makedirs(runDirectory)
-
-#Print file splits to runDirectory
-fileDF.to_csv(runDirectory+'/fileSplits,csv')
-
-
-################################
-#####Perform group analysis#####
-################################
-
-#Create paramList
-paramList=[[groupId,fileList,targetWordCount,cocoWindow,svdInt,cvWindow,simCount] for groupId in subgroupList]
-
-#Parallelize calulation
-timeIn=time.time()
-masterOutput=Parallel(n_jobs=mp.cpu_count()-1)(delayed(textAnalysis)(x) for x in paramList)  
-
-
-#Create output file
-outputDF=pd.DataFrame(masterOutput,columns=['groupId','perPos','perNeg','perPosDoc','perNegDoc','judgementCount','judgementFrac','avgSD'])
-outputDF.to_csv(runDirectory+'/masterOutput.csv')
+    fileDF=pd.DataFrame(fileList,columns=['group','filepath','subgroup'])
+    
+    
+    #Get set of subgroups
+    subgroupList=[ list(y) for y in set((x[0],x[2]) for x in fileList) ]
+    
+    #Make output directory
+    runDirectory='./pythonOutput/'+ time.strftime("%c")
+    os.makedirs(runDirectory)
+    
+    #Print file splits to runDirectory
+    fileDF.to_csv(runDirectory+'/fileSplits.csv')
+    
+    
+    ################################
+    #####Perform group analysis#####
+    ################################
+    
+    #Create paramList
+    paramList=[[x,fileList,targetWordCount,cocoWindow,svdInt,cvWindow,simCount] for x in subgroupList]
+    
+    #Parallelize calulation
+#    masterOutput=Parallel(n_jobs=nCores)(delayed(textAnalysis)(x) for x in paramList)  
+    xPool=mp.Pool(processes=4)    
+    outputList=[xPool.apply_async(textAnalysis, args=(x,)) for x in paramList]
+    masterOutput=[p.get() for p in outputList]    
+    #Create output file
+    outputDF=pd.DataFrame(masterOutput,columns=['groupId','perPos','perNeg','perPosDoc','perNegDoc','judgementCount','judgementFrac','avgSD'])
+    outputDF.to_csv(runDirectory+'/masterOutput.csv')
 
 
 
+#Set inital conditions and run
+if __name__ == '__main__':
+    rawPath = './data_dsicap/'
+    groupList=['DorothyDay','JohnPiper','MehrBaba','NaumanKhan','PastorAnderson',
+               'Rabbinic','Shepherd','Unitarian','WBC']
+    groupSize=10
+    testSplit=0.1
+    targetWordCount=10
+    cocoWindow=6
+    svdInt=50
+    cvWindow=6
+    simCount=1000
+    nCores=mp.cpu_count() #39 minutes
+#    nCores=1
+#    
+    
+    startTime=time.time()
+    runMaster(rawPath,groupList,groupSize,testSplit,targetWordCount,cocoWindow,svdInt,cvWindow,simCount,nCores)
+    endTime=time.time()
+    print((endTime-startTime)/60)#.82
 
